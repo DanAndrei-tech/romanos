cfb279985e17f0a7dd22436a2c034e951ea42194
DanAndrei-tech <detesan7@gmail.com>
2023-03-18 19:41:53 +0100

    commit andrei

--- main.py
+++ main.py
@@ -2,47 +2,46 @@
 class RomanNumberError(Exception):
     pass
 
-diccionario = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-
-unidades = {
-    1: 'I',2: 'II', 3: 'III',
+#diccionario = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+dic_entero_a_romanos={
+    1:'I', 2:'II', 3:'III',
     4: 'IV', 5: 'V', 6:'VI',
-    7:'VII', 8:'VIII', 9:'IX'
-}
-decenas= {
-    10: 'X',20: 'XX', 30: 'XXX',
+    7:'VII', 8:'VIII', 9:'IX',
+    10:'X',20:'XX', 30:'XXX',
     40: 'XL', 50: 'L', 60:'LX',
-    70:'LXX', 80:'LXXX', 90:'XC'
-}
-centenas = {
+    70:'LXX', 80:'LXXX', 90:'XC',
     100: 'C',200: 'CC', 300: 'CCC',
     400: 'CD', 500:'D' , 600:'DC',
-    700:'DCC', 800:'DCCC', 900:'CM'
-}
-millares={
+    700:'DCC', 800:'DCCC', 900:'CM',
     1000: 'M', 2000:'MM', 3000:'MMM'
 }
-
-
-
     
-
 def entero_a_romano(numero):
-    numero = str(numero) #transformar en cadena
+    #numero = str(numero) #transformar en cadena
+    numero = "{:0>4d}".format(numero)
     list_numero = list(numero) # transformar cadena en lista
+    valor_romano = ""
+    longitud = len(list_numero)
 
-    for i in range(0, len(list_numero)):       
-        if i == 0:
-            list_numero[i] = int(list_numero[i])*1000 
-        if i == 1:
-            list_numero[i] = int(list_numero[i])*100
-        if i == 2:
-            list_numero[i] = int(list_numero[i])*10
-        if i == 3:
-            list_numero[i] = int(list_numero[i])     
+    cont = 0
+    valor_num = 1000
+    while cont < len(list_numero):
+        list_numero[cont] = int(list_numero[cont])* valor_num
+        valor_romano += dic_entero_a_romanos.get(list_numero[cont], "")
+        cont+=1
+        valor_num /=10
 
-    return list_numero   
 
-print(entero_a_romano(1991))
-    
+    return valor_romano
+
+print(entero_a_romano(28))
+
+
+
+
+
+
+
+def romano_a_entero(valor:str)->int:
+    pass
 
--- test_entero_a_romano.py
+++ test_entero_a_romano.py
@@ -1,5 +1,12 @@
 
+from main import entero_a_romano
 
+valor_final = entero_a_romano(1994)
 
-def test_prueba():
-    assert 10 < 20
+#def test_prueba_entero_a_romano():
+    #assert valor_final == [1000,900,90,4]
+
+#test_prueba_entero_a_romano(valor_final)
+
+def test_prueba_entero_a_romano():
+    assert valor_final == 'MCMXCIV'
